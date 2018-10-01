5d9d9d2e10eb353883cf38d928dd8b9480ef6c7d
fsorace0 <43689912+fsorace0@users.noreply.github.com>
2018-10-01 06:57:29 -0400

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,21 @@
+.content {
+   background:#fff;
+   padding-top:2em;
+}
+.home .content {
+   padding-top:0;
+}
+
+.main { }
+.page-header {
+   margin: 10px 0 20px 0;
+   background: #222;
+   padding: 5px;
+   border-radius: 5px;
+}
+.page-header h1 {
+   color:#fff;
+   margin:0;
+}
+.page-header h1:before {
+   content:  "\00bb";
